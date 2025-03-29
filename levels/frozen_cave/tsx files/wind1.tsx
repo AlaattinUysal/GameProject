8c3cbbcd0e48f741b3f255b0a0c86c80c810e8e3
Berat Kayar <64198444+BeratKayar@users.noreply.github.com>
2025-03-29 16:35:40 +0300

    yeni tile set'ler

---
+++ levels/frozen_cave/tsx files/wind1.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="wind1" tilewidth="32" tileheight="32" tilecount="16" columns="16">
+ <image source="../snow and wind/Wind1.png" width="512" height="32"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="3" duration="2500"/>
+  </animation>
+ </tile>
+</tileset>
