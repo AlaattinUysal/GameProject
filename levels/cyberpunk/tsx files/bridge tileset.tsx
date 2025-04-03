49fd6958da0c44f43695f62452269d07c5129df6
Berat Kayar <64198444+BeratKayar@users.noreply.github.com>
2025-04-03 22:55:03 +0300

    Assetler Eklendi
    Kullanılması planlanan assetler eklendi

---
+++ levels/cyberpunk/tsx files/bridge tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="bridge tileset" tilewidth="32" tileheight="32" tilecount="187" columns="17">
+ <image source="../bridge set/Bridge_tileset.png" width="544" height="352"/>
+</tileset>
