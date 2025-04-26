3590f31fa03f73f19ae719660d5c8ef7ec9dbb8f
Berat Kayar <64198444+BeratKayar@users.noreply.github.com>
2025-04-26 13:49:47 +0300

    Delete levels/cyberpunk/tsx files directory

--- levels/cyberpunk/tsx files/industrial zone props.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="industrial zone props" tilewidth="32" tileheight="32" tilecount="200" columns="10">
- <image source="../industrial zone/industrial zone props.png" width="320" height="640"/>
-</tileset>
