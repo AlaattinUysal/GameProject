06dbd43b16b9f06b8f89f28376f198db09d18a01
Berat Kayar <64198444+BeratKayar@users.noreply.github.com>
2025-04-26 13:50:39 +0300

    tilesets files

---
+++ levels/cyberpunk/tsx files/bg5.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="bg5" tilewidth="32" tileheight="32" tilecount="720" columns="36">
+ <image source="../back ground/5_scaled_2x_pngcrushed.png" width="1152" height="648"/>
+</tileset>
---
+++ levels/cyberpunk/tsx files/power station props.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="power station props" tilewidth="32" tileheight="32" tilecount="200" columns="10">
+ <image source="../power station/power station props.png" width="320" height="640"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
