beb2f22553935b61399434c5fbab668720ab945c
Özer Koca <153188071+ozerkoca@users.noreply.github.com>
2025-05-09 17:53:22 +0300

    Create config.py

---
+++ EnemyFile/config.py
@@ -0,0 +1,15 @@
+import pygame
+
+# Window settings
+GENISLIK = 1600
+YUKSEKLIK = 900
+FPS = 60
+
+# State constants
+DEVRIYE = "devriye"
+TAKIP = "takip"
+SALDIRI = "saldiri"
+
+# Font
+pygame.font.init()
+FONT = pygame.font.SysFont('Arial', 24)
