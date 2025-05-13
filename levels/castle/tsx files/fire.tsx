485b5760370d3c67d37f4f933ece12a2bebc685d
Berat Kayar <64198444+BeratKayar@users.noreply.github.com>
2025-05-14 01:39:25 +0300

    tuzak ve iksir ayarlamaları yapıldı

---
+++ levels/castle/tsx files/fire.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="fire" tilewidth="48" tileheight="48" tilecount="20" columns="5">
+ <image source="../props/Medieval_animated_props_spritesheet_final_01.png" width="240" height="192"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <animation>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ levels/lab/tsx files/lab ts.tsx
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="lab ts" tilewidth="32" tileheight="32" tilecount="400" columns="20">
+ <image source="../lab ts/lab ts.png" width="640" height="640"/>
+ <tile id="206">
+  <animation>
+   <frame tileid="206" duration="100"/>
+   <frame tileid="207" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="226">
+  <animation>
+   <frame tileid="226" duration="50"/>
+   <frame tileid="227" duration="50"/>
+   <frame tileid="228" duration="50"/>
+   <frame tileid="229" duration="50"/>
+   <frame tileid="230" duration="50"/>
+   <frame tileid="231" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="246">
+  <animation>
+   <frame tileid="246" duration="100"/>
+   <frame tileid="247" duration="100"/>
+   <frame tileid="248" duration="100"/>
+   <frame tileid="249" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="280">
+  <animation>
+   <frame tileid="280" duration="100"/>
+   <frame tileid="281" duration="100"/>
+   <frame tileid="282" duration="100"/>
+   <frame tileid="283" duration="100"/>
+   <frame tileid="284" duration="100"/>
+   <frame tileid="285" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
