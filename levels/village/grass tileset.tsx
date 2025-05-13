7302ad9b190de97b4bfa135d0b2e21025a8af7c3
Berat Kayar <64198444+BeratKayar@users.noreply.github.com>
2025-05-14 01:40:52 +0300

    iksir ve tuzak ayarlamaları yapıldı

---
+++ levels/village/grass tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="grass tileset" tilewidth="48" tileheight="48" tilecount="2800" columns="80">
+ <image source="village.png" width="3840" height="1680"/>
+</tileset>
---
+++ levels/village/prtls.tsx
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="prtls" tilewidth="48" tileheight="48" tilecount="24" columns="12">
+ <image source="portal.png" width="576" height="96"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="125"/>
+   <frame tileid="2" duration="125"/>
+   <frame tileid="4" duration="125"/>
+   <frame tileid="6" duration="125"/>
+   <frame tileid="8" duration="125"/>
+   <frame tileid="10" duration="125"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="125"/>
+   <frame tileid="3" duration="125"/>
+   <frame tileid="5" duration="125"/>
+   <frame tileid="7" duration="125"/>
+   <frame tileid="9" duration="125"/>
+   <frame tileid="11" duration="125"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <animation>
+   <frame tileid="12" duration="125"/>
+   <frame tileid="14" duration="125"/>
+   <frame tileid="16" duration="125"/>
+   <frame tileid="18" duration="125"/>
+   <frame tileid="20" duration="125"/>
+   <frame tileid="22" duration="125"/>
+  </animation>
+ </tile>
+ <tile id="13">
+  <animation>
+   <frame tileid="13" duration="125"/>
+   <frame tileid="15" duration="125"/>
+   <frame tileid="17" duration="125"/>
+   <frame tileid="19" duration="125"/>
+   <frame tileid="21" duration="125"/>
+   <frame tileid="23" duration="125"/>
+  </animation>
+ </tile>
+</tileset>
