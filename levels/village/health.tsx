ccc1aa7804d43870af807b7fe38f23f7352f66f0
Berat Kayar <64198444+BeratKayar@users.noreply.github.com>
2025-05-14 01:27:11 +0300

    Add files via upload

---
+++ levels/village/health.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="health" tilewidth="48" tileheight="48" tilecount="9" columns="3">
+ <image source="health/health 48x48.png" width="144" height="144"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="150"/>
+   <frame tileid="1" duration="150"/>
+   <frame tileid="2" duration="150"/>
+   <frame tileid="3" duration="150"/>
+   <frame tileid="4" duration="150"/>
+   <frame tileid="5" duration="150"/>
+   <frame tileid="6" duration="150"/>
+   <frame tileid="7" duration="150"/>
+  </animation>
+ </tile>
+</tileset>
