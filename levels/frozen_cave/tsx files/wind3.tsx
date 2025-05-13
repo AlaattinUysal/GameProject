99f2b0ca1439f86a228384eb78b10adc2f8c2082
Berat Kayar <64198444+BeratKayar@users.noreply.github.com>
2025-05-14 01:46:31 +0300

    iksir ve tuzak ayarlamaları

---
+++ levels/frozen_cave/tsx files/wind3.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="wind3" tilewidth="32" tileheight="32" tilecount="16" columns="16">
+ <image source="../snow and wind/Wind4.png" width="512" height="32"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="5" duration="2150"/>
+  </animation>
+ </tile>
+</tileset>
