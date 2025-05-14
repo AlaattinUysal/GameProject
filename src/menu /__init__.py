f463de467cb114cb47be09bee1437e7463ef4133
A.Uysal <144958436+AlaattinUysal@users.noreply.github.com>
2025-05-14 15:23:03 +0300

    Delete src/menu /__init__.py

--- src/menu /__init__.py
+++
@@ -1,6 +0,0 @@
-# menu/__init__.py
-
-from .menu_logic import Menu
-from .constants import *
-from .assets import load_background, load_samurai_characters, load_font
-from .ui import PixelButton, LevelCompleteButton, Slider, TextInputBox, ShopSkill
