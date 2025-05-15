5cc0c005b7a1fa04a4824b55a2b16b5b418e080f
Özer Koca <153188071+ozerkoca@users.noreply.github.com>
2025-05-15 16:51:36 +0300

    Update main.py

--- EnemyFile/main.py
+++ EnemyFile/main.py
@@ -14,6 +14,8 @@ saat = pygame.time.Clock()
 
 # Map loading
 tmx_data, map_genislik, map_yukseklik = load_map('C:/Users/özer/Desktop/sw_eng_project/sw_eng_project/levels/village/village.tmx')
+#tmx_data, map_genislik, map_yukseklik = load_map('C:/Users/özer/Desktop/sw_eng_project/sw_eng_project/levels/cyberpunk/cyberpunk.tmx')
+#tmx_data,map_genislik,map_yukseklik=load_map('C:/Users/özer/Desktop/sw_eng_project/sw_eng_project/levels/frozen_cave/frozen cave.tmx')
 collision_rects = get_collision_rects(tmx_data)
 player_spawn = get_player_spawn(tmx_data)
 
@@ -22,13 +24,13 @@ ana_karakter = AnaKarakter(*player_spawn)
 
 # Enemy list
 dusman_listesi = [
-    #enemy_types.NinjaMonk(300, 1200),
+    enemy_types.NinjaMonk(300, 1200),
     #enemy_types.Wizard(400, 1200),
     #enemy_types.NinjaPeasant(500, 1200),
     #enemy_types.KarasuTengu(800, 1200),
     #enemy_types.Kitsune(1000, 1200),
     #enemy_types.YamabushiTengu(1200, 1200),
-    #enemy_types.BlackWolf(1400, 1200),
+    enemy_types.BlackWolf(1400, 1200),
     #enemy_types.RedWolf(1600, 1200),
     #enemy_types.WhiteWolf(1800, 1200),
     #enemy_types.Minotaur1(2000, 1200),
@@ -40,6 +42,59 @@ dusman_listesi = [
     #enemy_types.Gladiator1(3200, 1200),
     #enemy_types.Gladiator2(3400, 1200),
     #enemy_types.Gladiator3(3600, 1200),
+    #enemy_types.Amazon1(3000,1200),
+    #enemy_types.Amazon2(3000,1200),
+    #enemy_types.Amazon3(3000,1200),
+    #enemy_types.AnimeKnight(2000,1200),
+    #enemy_types.BattleMecha1(1200,1200),
+    #enemy_types.BattleMecha2(1000,1200),
+    #enemy_types.BattleMecha3(1200,1200),
+    #enemy_types.Berserk(1000,1200),
+    #enemy_types.BrownOrc(500,1200),
+    #enemy_types.CityMan1(600,1200),
+    #enemy_types.CityMan2(700,1200),
+    #enemy_types.CityMan3(800,1200),
+    #enemy_types.Demon1(500,1200),
+    #enemy_types.Demon2(1500,1200),
+    #enemy_types.Demon3(2000,1200),
+    #enemy_types.Demon4(3000,1200),
+    #enemy_types.Demon5(1800,1200),
+    ##enemy_types.Demon6(2700,1200),
+    #enemy_types.Enchantress(2900,1200),
+    #enemy_types.Goblin1(3000,1200),
+    #enemy_types.Goblin2(3200,1200),
+    #enemy_types.Goblin3(1600,1200),
+    #enemy_types.GoblinKing(1700,1200),
+    #enemy_types.Gorgon1(2500,1200),
+    #enemy_types.Gorgon2(2300,1200),
+    #enemy_types.Gorgon3(3100,1200),
+    #enemy_types.GreenOrc(2800,1200),
+    #enemy_types.Hellhound1(3500,1200),
+    #enemy_types.Hellhound2(3200,1200),
+    #enemy_types.Hellhound3(3100,1200),
+    #enemy_types.HugeMushroom(2600,1200),
+    #enemy_types.Knight1(3000,1200),
+    #enemy_types.Knight2(3000,1200),
+    #enemy_types.Knight3(3000,1200),
+    #enemy_types.Lightning(3300,1200),
+    #enemy_types.Man1(1200,1200),
+    #enemy_types.Man2(1200,1200),
+    #enemy_types.Man3(1200,1200),
+    #enemy_types.Mutant1(1000,1200),
+    #enemy_types.Mutant2(900,1200),
+    #enemy_types.Mutant3(2300,1200),
+    #enemy_types.Pyromancer1(1500,1200),
+    #enemy_types.Pyromancer2(1800,1200),
+    #enemy_types.Pyromancer3(1900,1200),
+    #enemy_types.Raider1(3500,1200),
+    #enemy_types.Raider3(3200,1200),
+    #enemy_types.VampireBat(1400,1200),
+    #enemy_types.Samurai(700,1200),
+    #enemy_types.SamuraiCommander(1700,1200),
+    #enemy_types.Witch1(2500,1200),
+    #enemy_types.Witch2(2400,1200),
+    #enemy_types.Witch3(2600,1200),
+    #enemy_types.WomanOrc(2600,1200),
 ]
 
 # Camera settings
