493da28791747370822ef0990dfbe6e3af1b36f8
A.Uysal <144958436+AlaattinUysal@users.noreply.github.com>
2025-05-27 22:49:08 +0300

    Add files via upload

---
+++ levels/cyberpunk/tsx files/industrial zone tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="industrial zone tileset" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="../industrial zone/industrial zone tileset.png" width="320" height="320"/>
+</tileset>
